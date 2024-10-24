f096522f7defc8c18451ee916e94f974adb25a75
letMoonRunMyMood <1003251453@qq.com>
2024-10-24 22:01:10 +0800

    Update Test5.java

--- Test5.java
+++ Test5.java
@@ -54,7 +54,45 @@ public class Test5 {
         return output;
     }
 
+    public int[]CBC_decryption(int[]cryption,int[]k,int[]array){
+        Encryption e=new Encryption();
+        int len=cryption.length;
+        //将temp初始化成cryption的nx16矩阵,并且将最后一行直接转为第一次计算后的明文
+        int[][]temp=new int[len/16][16];
 
+        //原始密钥的副本
+        int[][]Sub_cryption=new int[len/16][16];
+        for (int i = 0; i <len/16 ; i++) {
+            for (int j = 0; j < 16; j++) {
+                temp[i][j]=cryption[i*16+j];
+            }
+        }
+
+        for (int i = 0; i <len/16 ; i++) {
+            for (int j = 0; j < 16; j++) {
+                    Sub_cryption[i][j] = cryption[i * 16 + j];
+            }
+        }
+
+        temp[0]=e.Decrypt(temp[0],k);
+        temp[0]=Xor(temp[0],array);
+
+        //从第第二行开始迭代
+        for (int i = 1; i <len/16; i++) {
+            temp[i]=e.Decrypt(temp[i],k);
+            temp[i]=Xor(temp[i],Sub_cryption[i-1]);
+        }
+
+        int[]output=new int[len];
+        //将temp铺平
+        for (int i = 0; i <len/16 ; i++) {
+            for (int j = 0; j <16 ; j++) {
+                output[i*16+j]=temp[i][j];
+            }
+        }
+
+        return output;
+    }
     void CBC(){
         System.out.println("CBC加密模式");
         System.out.print("输入初始向量：");
@@ -76,12 +114,21 @@ public class Test5 {
             throw new IllegalArgumentException("明文必须被16整除，密钥和初始向量必须为16位");
         }
         int[]cryption=CBC_encryption(plaintext0,key0,array0);
-
+        int[]plaintext_out=CBC_decryption(cryption,key0,array0);
         System.out.println("CBC模式加密后的密文为："+ Arrays.toString(cryption));
+        System.out.println("CBC模式解密后的明文为："+ Arrays.toString(plaintext_out));
+
+        System.out.print("请输入篡改后的密文：");
+        Scanner sc4=new Scanner(System.in);
+        String cryption0=sc4.next();
+        int[]cryption_out=Stringtoint(cryption0);
+        int[]plain_alter=CBC_decryption(cryption_out,key0,array0);
+        System.out.println("CBC模式解密篡改后的密文的明文为："+ Arrays.toString(plain_alter));
+
 
     }
     public static void main(String[] args) {
-        Test5 test5=new Test5();
+         Test5 test5=new Test5();
         Encryption e=new Encryption();
         test5.CBC();
     }
